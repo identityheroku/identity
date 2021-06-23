75c690053e8c02b43499db29f0a514102cff0090
onurteber <teberonur00@gmail.com>
2021-06-23 22:42:49 +0300

    first

---
+++ IdentityApi/Controllers/LoginController.cs
@@ -0,0 +1,99 @@
+﻿using IdentityApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+        [HttpGet]
+        public IActionResult Login(string username, string password)
+        {
+            UserModel login = new UserModel();
+            login.UserName = username;
+            login.Password = password;
+            IActionResult response = Unauthorized();
+
+            var user = AuthenticateUser(login);
+
+            if(user != null)
+            {
+                var tokenStr = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenStr });
+            }
+
+            return response;
+
+        }
+
+        private string GenerateJSONWebToken(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Email, user.EmailAddress),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                    issuer: _config["Jwt:Issuer"],
+                    audience: _config["Jwt:Issuer"],
+                    claims,
+                    expires: DateTime.Now.AddMinutes(5),
+                    signingCredentials: credentials
+                    );
+
+            var encodetoken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodetoken;
+
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+            if(login.UserName == "sa" && login.Password == "123")
+            {
+                user = new UserModel
+                {
+                    UserName = "sa",
+                    Password = "123",
+                    EmailAddress = "sa@sa.com"
+                };
+            }
+            return user;
+        }
+
+        [Authorize]
+        [HttpPost("Post")]
+        public string Post()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            IList<Claim> claims = identity.Claims.ToList();
+            var userName = claims[0].Value;
+            return userName;
+        }
+    }
+}
---
+++ IdentityApi/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace IdentityApi.Models
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
